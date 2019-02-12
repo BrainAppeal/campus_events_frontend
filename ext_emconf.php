6f2f876da628717f848bd6665c8cbbd2a8920890
Joshua Billert <Joshua.Billert@Brain-Appeal.com>
2019-02-12 16:26:36 +0100

    Update version constraints and dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,20 @@
 <?php
 
+/** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Campus Events Frontend',
     'description' => 'Frontend plugins for Campus Events',
     'category' => 'plugin',
     'author' => 'Gert Hammes',
-    'author_email' => 'gert.hammes@brain-appeal.com',
     'author_company' => 'Brain Appeal GmbH',
-    'state' => 'alpha',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'author_email' => 'info@brain-appeal.com',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.9.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
-            'campus_events_connector' => '>=0.4.0',
+            'campus_events_connector' => '>=0.9.0',
         ],
         'conflicts' => [],
         'suggests' => [],
