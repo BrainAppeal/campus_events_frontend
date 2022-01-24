910559dd87b4e6cfa8ea8960c683c54d28ec48f0
Gert Hammes <gert.hammes@brain-appeal.com>
2022-01-24 14:59:24 +0100

    [FEATURE] add compatibility to API > 2.27.0 #37671

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.1-10.4.99',
-            'campus_events_connector' => '>=1.0.1',
+            'campus_events_connector' => '1.0.1-2.999.99',
         ],
         'conflicts' => [],
         'suggests' => [],
