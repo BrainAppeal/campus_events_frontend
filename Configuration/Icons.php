149190adebf876e8c61f123fd4d1be110df41ba5
BrainAppeal <info@brain-appeal.com>
2023-04-24 16:00:00 +0200

    Release 3.0.3 (filter frontend list by CE filter categories; improve timespan view helper)  https://www.campus-events.com/

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'campus_events_frontend-plugin-event' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:campus_events_frontend/Resources/Public/Icons/calendar-days-solid.svg',
+    ],
+];
